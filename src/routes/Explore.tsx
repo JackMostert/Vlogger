c49104ff0f90524b6e3d3a98766d266ba4cbe79f
j_mostert <j_mostert@outlook.com>
2020-05-10 22:29:07 +0100

    added explore page

--- src/routes/Explore.tsx
+++ src/routes/Explore.tsx
@@ -1,11 +1,42 @@
 import * as React from "react";
+import Icon from "../components/Icon/Icon";
+import HashTag from "../components/Cards/HashTag/HashTag";
 
 interface IExploreProps {
   route: any;
 }
 
+const Ghashtags = () => {
+  let AH = [];
+  for (let i = 0; i < 30; i++) {
+    AH.push(
+      <HashTag
+        hashtag="#happy feelings"
+        url="https://picsum.photos/200/200"
+        onClick={(hashtag: string) => {}}
+      />
+    );
+  }
+  return AH;
+};
+
 const Explore: React.FunctionComponent<IExploreProps> = (props) => {
-  return <h1>Explore</h1>;
+  return (
+    <div className="page page-explore">
+      <div className="page-explore__form-container">
+        <Icon
+          iconName="las la-search page-explore__input-field-icon"
+          iconSize={29}
+        />
+        <input
+          className="page-explore__input-field"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+      <div className="page-explore__hashtag-grid">{Ghashtags()}</div>
+    </div>
+  );
 };
 
 export default Explore;
